967f3075fcfa3a0f6bd1c23e6a887b3f6de9d2c9
skapxd <skapxd@users.noreply.github.com>
2020-10-03 03:18:50 +0000

    deploy: 458441f22dddabff67c7f48a85f205c240c4f7d9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "29a5ac4fbf16070b3226b022a1710b8c",
-"assets/NOTICES": "ee46c3b5c9f21ead637e489c91bfd715",
+  "main.dart.js": "d7f473b972803a483879104228b80e13",
+"assets/NOTICES": "2ed220557167d5ff55b68864f4e73074",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "4bec0d3bc7d873522fb4ea6e96b019d5",
-"/": "4bec0d3bc7d873522fb4ea6e96b019d5",
+"index.html": "e181872e5acc32cc4c84fdf3ee307443",
+"/": "e181872e5acc32cc4c84fdf3ee307443",
 "manifest.json": "d2858d11acb3fb8e04739cbce06e7d59",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
